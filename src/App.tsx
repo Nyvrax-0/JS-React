56607cda561666f50d2ba71aad5f8b3702d2126f
Ibrohim <shaxabidinovibrohim@gmail.com>
2026-03-01 00:01:32 +0600

    описание изменений

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,38 @@
-import { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState<string[]>([]);
+
+  function addTask() {
+    if (task === "") return;
+    setTasks(tasks.concat(task));
+    setTask("");
+  }
+
+  function deleteTask(index: number) {
+    setTasks(tasks.filter((_, i) => i !== index));
+  }
 
   return (
-    <>
-      <h1>Beknur</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count + 1)}>count is {count}</button>
-      </div>
-    </>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Todo List</h1>
+
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button onClick={addTask}>Добавить</button>
+
+      <ul>
+        {tasks.map((item, index) => (
+          <li key={index}>
+            {item} <button onClick={() => deleteTask(index)}>❌</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
